ec6fa1490ca3703e6fd2b1511ad2d9d99818b0ef
Pietro2302 <pietro.massari@mail.polimi.it>
2024-10-18 05:26:51 -0400

    A

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,8 +18,9 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const ISBN = req.params.isbn;
+    let filtered_books = books.filter((books) => books.isbn === ISBN);
+    res.send(filtered_books);
  });
   
 // Get book details based on author
